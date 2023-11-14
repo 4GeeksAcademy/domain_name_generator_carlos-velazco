45d4e46edfc4d088c6a16cf4ed22fc1904c7bb1a
velazcopolo <144776724+velazcopolo@users.noreply.github.com>
2023-11-14 16:06:51 +0000

    actualizacion de variables en console.log

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ for (let i = 0; i < pronoun.length; i++) {
     for (let j = 0; j < adj.length; j++) {
         
         for (let k = 0; k < noun.length; k++) {
-            console.log(pronoun[j] + adj[i] + noun[k] + ".com");
-            console.log(pronoun[j] + adj[i] + noun[k] + ".es");
-            console.log(pronoun[j] + adj[i] + noun[k] + ".net");
+            console.log(pronoun[i] + adj[j] + noun[k] + ".com");
+            console.log(pronoun[i] + adj[j] + noun[k] + ".es");
+            console.log(pronoun[i] + adj[j] + noun[k] + ".net");
 
         }
         
